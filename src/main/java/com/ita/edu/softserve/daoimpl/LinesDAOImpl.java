bf4987bc3c629c59cb55ca4044a7c7e0d47b4828
MPS <MPS@MPS-PC>
2014-01-16 21:21:48 +0200

    edit LinesDAOImpl Копіпаст - погана штука

--- src/main/java/com/ita/edu/softserve/daoimpl/LinesDAOImpl.java
+++ src/main/java/com/ita/edu/softserve/daoimpl/LinesDAOImpl.java
@@ -4,7 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.ita.edu.softserve.daoiface.AbstractDAOClass;
-import com.ita.edu.softserve.daoiface.RoutesDAO;
+import com.ita.edu.softserve.daoiface.LinesDAO;
+import com.ita.edu.softserve.entity.Lines;
 import com.ita.edu.softserve.entity.Routes;
 
 /**
@@ -12,31 +13,31 @@ import com.ita.edu.softserve.entity.Routes;
  * @author iryna
  *
  */
-public class LinesDAOImpl extends AbstractDAOClass implements RoutesDAO{
+public class LinesDAOImpl extends AbstractDAOClass implements LinesDAO{
 
 	@PersistenceContext(name = PERSISTENCE_UNIT_NAME)
     private EntityManager entityManager;
 	
 	@Override
-	public Routes findByCode(String routeCode) {
+	public Lines findByName(String lineName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void save(Routes route) {
+	public void save(Lines lines) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void remove(Routes route) {
+	public void remove(Lines lines) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Routes update(Routes route) {
+	public Lines update(Lines lines) {
 		// TODO Auto-generated method stub
 		return null;
 	}
