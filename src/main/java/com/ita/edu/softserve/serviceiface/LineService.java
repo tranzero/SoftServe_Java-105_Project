47aca6bb2eadd310b5fc23ca81617572fef0c538
MPS <MPS@MPS-PC>
2014-01-16 21:31:00 +0200

    Add LineService and LineServiceImpl

---
+++ src/main/java/com/ita/edu/softserve/serviceiface/LineService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ita.edu.softserve.serviceiface;
+
+import java.util.List;
+
+import com.ita.edu.softserve.entity.Lines;
+
+/**
+ * @author MPS
+ *
+ */
+public interface LineService {
+	
+	public List<Lines> getAllLines();
+
+}
---
+++ src/main/java/com/ita/edu/softserve/serviceimpl/LineServiceImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ita.edu.softserve.serviceimpl;
+
+import java.util.List;
+
+import com.ita.edu.softserve.entity.Lines;
+import com.ita.edu.softserve.serviceiface.LineService;
+
+/**
+ * @author MPS
+ *
+ */
+public class LineServiceImpl implements LineService{
+
+	@Override
+	public List<Lines> getAllLines() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
