33590aa34a7ee134624b4c28989a4587c11b7685
Роман <@roman>
2014-01-19 23:18:47 +0200

    Add interface StationsService.

---
+++ src/main/java/com/ita/edu/softserve/service/StationsService.java
@@ -0,0 +1,18 @@
+package com.ita.edu.softserve.service;
+
+import java.util.List;
+
+import com.ita.edu.softserve.entity.Stations;
+
+/**
+ * Service interface for stations.
+ * @author Роман
+ */
+public interface StationsService {
+	/**
+	 * Get list of Stations.
+	 * @author Роман
+	 */
+	public List<Stations> findAllStation();
+
+}
