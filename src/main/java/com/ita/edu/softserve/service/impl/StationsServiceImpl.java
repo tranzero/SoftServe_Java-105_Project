f9f7e400d78a4970274a0b875c022dc95a1e7193
rparatc <RomeXak@gmail.com>
2014-01-22 21:49:54 +0200

    Add two constructors.

--- src/main/java/com/ita/edu/softserve/service/impl/StationsServiceImpl.java
+++ src/main/java/com/ita/edu/softserve/service/impl/StationsServiceImpl.java
@@ -9,20 +9,43 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ita.edu.softserve.dao.impl.StationsDAOImpl;
 import com.ita.edu.softserve.entity.Stations;
 import com.ita.edu.softserve.service.StationsService;
+
+/**
+ * This is station service class.
+ * @author Роман
+ * 
+ */
 @Service
 public class StationsServiceImpl implements StationsService {
-	
+	/**
+	 * 
+	 */
 	@Autowired
 	private StationsDAOImpl stationDao;
-	
+
 	/**
-	 * Obtain list of all stations.
+	 * Constructor without arguments.
+	 */
+	public StationsServiceImpl() {
+	}
+
+	/**
+	 * Constructor with one argument.
+	 * @param stationDao
+	 */
+	public StationsServiceImpl(StationsDAOImpl stationDao) {
+		this.stationDao = stationDao;
+	}
+
+	/**
+	 * Return list of all stations.
+	 * 
 	 * @author Roman
 	 */
 	@Transactional
 	@Override
 	public List<Stations> findAllStations() {
-		 return stationDao.getAllEntities();
+		return stationDao.getAllEntities();
 	}
 
 }
