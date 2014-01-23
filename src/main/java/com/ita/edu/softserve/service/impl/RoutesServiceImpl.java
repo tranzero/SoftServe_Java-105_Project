f056280eca16b55d5b6198aca3087d99f5952b30
LyubomyrGitHub <pentsko@gmail.com>
2014-01-23 10:42:09 +0200

    Edit tests

--- src/main/java/com/ita/edu/softserve/service/impl/RoutesServiceImpl.java
+++ src/main/java/com/ita/edu/softserve/service/impl/RoutesServiceImpl.java
@@ -104,7 +104,10 @@ public class RoutesServiceImpl implements RoutesService {
 			throw new IllegalArgumentException(
 					"idStationDeparting should be greater than zero");
 		}
-
+		if (timeDepartureMin.after(timeDepartureMax)) {
+			throw new IllegalArgumentException(
+					"timeDepartureMax should be greater or equals than timeDepartureMin");
+		}
 		List<Routes> routersListByStationDeparting = new ArrayList<Routes>();
 		StopsDAOImpl stopsDao = new StopsDAOImpl();
 		StationsOnLineDAOImpl stationsOnLineDao = new StationsOnLineDAOImpl();
