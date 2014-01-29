3ef93ba302546cadbf2b1c5030961af4cd06fb2b
Anastasiia <Anastasiia@Anastasiia-PC>
2014-01-30 01:31:23 +0200

    Edit LinesManager

--- src/main/java/com/ita/edu/softserve/manager/LinesManager.java
+++ src/main/java/com/ita/edu/softserve/manager/LinesManager.java
@@ -12,13 +12,14 @@ import com.ita.edu.softserve.entity.Stations;
  * 
  * @author yuraloga
  * @author MPS
+ * @author pankivanastasiia
  * 
  */
-public interface LinesManager extends BaseManager {
+public interface LinesManager extends BaseManager { 
 	
 	public List<Lines> getFullLines();
 	
-	public List<Lines> getLinesByStation(Stations station);
+	public List<Lines> getLinesByStation(String stationName);
 	
 	public List<Lines> getLinesTwoStationsCertainOrder(String stationName1,
 			String stationName2);
