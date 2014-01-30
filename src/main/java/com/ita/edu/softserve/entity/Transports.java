5c6edbb8c0086ed8a0f262badc57c4f63cf3dfee
tranzero <tranzero@192.168.1.104>
2014-01-30 19:22:05 +0200

    fix truble in Transports

--- src/main/java/com/ita/edu/softserve/entity/Transports.java
+++ src/main/java/com/ita/edu/softserve/entity/Transports.java
@@ -26,198 +26,86 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 @NamedQuery(name = Transports.TRANSPORTS_FIND_ALL, query = Transports.TRANSPORTS_FIND_ALL_QUERY)
 public class Transports extends BaseEntity {
 
-	static final String TRANSPORTS_FIND_ALL = "Transports.findAll";
-	static final String TRANSPORTS_FIND_ALL_QUERY = "SELECT t FROM Transports t";
+        static final String TRANSPORTS_FIND_ALL = "Transports.findAll";
+        static final String TRANSPORTS_FIND_ALL_QUERY = "SELECT t FROM Transports t";
 
-	@Id
-	@Column(name = "TRANSPORTID", nullable = false)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int transportId;
+        @Id
+        @Column(name = "TRANSPORTID", nullable = false)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int transportId;
 
-	@Column(name = "TRANSPORTCODE", nullable = false, length = 20)
-	private String code;
+        @Column(name = "TRANSPORTCODE", nullable = false, length = 20)
+        private String transportCode;
 
-	@Column(name = "STARTTIME", nullable = false)
-	private Time starttime;
+        @Column(name = "STARTTIME", nullable = false)
+        private Time startTime;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ROUTEID")
-	private Routes routes;
+        @ManyToOne(fetch = FetchType.EAGER)
+        @JoinColumn(name = "ROUTEID")
+        private Routes routes;
 
-	public Transports() {
-		super();
-	}
+        public Transports() {
+                super();
+        }
 
-	public Transports(int transportid, String code, Time starttime, int route) {
-		this();
-		this.transportId = transportid;
-		this.code = code;
-		this.starttime = starttime;
-		// this.route = route;
-	}
+        public Transports(int transportId, String transportCode, Time startTime) {
+                this();
+                this.transportId = transportId;
+                this.transportCode = transportCode;
+                this.startTime = startTime;
+        }
 
-	public int getTransportid() {
-		return this.transportId;
-	}
+        public int getTransportId() {
+                return transportId;
+        }
 
-	public void setTransportid(int transportid) {
-		this.transportId = transportid;
-	}
+        public void setTransportId(int transportId) {
+                this.transportId = transportId;
+        }
 
-	public String getCode() {
-		return this.code;
-	}
+        public String getTransportCode() {
+                return transportCode;
+        }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+        public void setTransportCode(String transportCode) {
+                this.transportCode = transportCode;
+        }
 
-	public Time getStarttime() {
-		return this.starttime;
-	}
+        public Time getStartTime() {
+                return startTime;
+        }
 
-	public void setStarttime(Time starttime) {
-		this.starttime = starttime;
-	}
+        public void setStartTime(Time startTime) {
+                this.startTime = startTime;
+        }
 
-	public Routes getRoutes() {
-		return routes;
-	}
+        public Routes getRoutes() {
+                return routes;
+        }
 
-	public void setRoutes(Routes param) {
-		this.routes = param;
-	}
+        public void setRoutes(Routes routes) {
+                this.routes = routes;
+        }
 
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(transportId)
-		// .append(code)
-				.hashCode();
-	}
+        @Override
+        public int hashCode() {
+                return new HashCodeBuilder().append(transportId)
+                // .append(code)
+                                .hashCode();
+        }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Transports other = (Transports) obj;
+        @Override
+        public boolean equals(Object obj) {
+                if (this == obj)
+                        return true;
+                if (obj == null)
+                        return false;
+                if (getClass() != obj.getClass())
+                        return false;
+                Transports other = (Transports) obj;
 
-		return new EqualsBuilder().append(transportId, other.transportId)
-		// .append(code, other.code)
-				.isEquals();
-	}
-}
-package com.ita.edu.softserve.entity;
-
-import java.sql.Time;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-
-/**
- * The persistent class for the transports database table.
- * 
- * @author Roman
- */
-@Entity
-@Table(name = "transports")
-@NamedQuery(name = Transports.TRANSPORTS_FIND_ALL, query = Transports.TRANSPORTS_FIND_ALL_QUERY)
-public class Transports extends BaseEntity {
-
-	static final String TRANSPORTS_FIND_ALL = "Transports.findAll";
-	static final String TRANSPORTS_FIND_ALL_QUERY = "SELECT t FROM Transports t";
-
-	@Id
-	@Column(name = "TRANSPORTID", nullable = false)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int transportId;
-
-	@Column(name = "TRANSPORTCODE", nullable = false, length = 20)
-	private String transportCode;
-
-	@Column(name = "STARTTIME", nullable = false)
-	private Time startTime;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ROUTEID")
-	private Routes routes;
-
-	public Transports() {
-		super();
-	}
-
-	public Transports(int transportId, String transportCode, Time startTime) {
-		this();
-		this.transportId = transportId;
-		this.transportCode = transportCode;
-		this.startTime = startTime;
-	}
-
-	public int getTransportId() {
-		return transportId;
-	}
-
-	public void setTransportId(int transportId) {
-		this.transportId = transportId;
-	}
-
-	public String getTransportCode() {
-		return transportCode;
-	}
-
-	public void setTransportCode(String transportCode) {
-		this.transportCode = transportCode;
-	}
-
-	public Time getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Time startTime) {
-		this.startTime = startTime;
-	}
-
-	public Routes getRoutes() {
-		return routes;
-	}
-
-	public void setRoutes(Routes routes) {
-		this.routes = routes;
-	}
-
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(transportId)
-		// .append(code)
-				.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Transports other = (Transports) obj;
-
-		return new EqualsBuilder().append(transportId, other.transportId)
-		// .append(code, other.code)
-				.isEquals();
-	}
+                return new EqualsBuilder().append(transportId, other.transportId)
+                // .append(code, other.code)
+                                .isEquals();
+        }
 }
