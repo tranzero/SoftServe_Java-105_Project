a61ef76d34647a73e2a5de1dc05099ad324f5e05
iryna <iryna@IRYNA-PC>
2014-01-31 02:00:54 +0200

    add method editUser(usname)

--- src/main/java/com/ita/edu/softserve/web/UserController.java
+++ src/main/java/com/ita/edu/softserve/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ita.edu.softserve.manager.UserManager;
 
@@ -18,23 +19,28 @@ import com.ita.edu.softserve.manager.UserManager;
 @Controller
 public class UserController {
 
-	 
-	 @Autowired
-	 private UserManager users;
-	 
-	 @RequestMapping(value = "userlist", method = RequestMethod.GET)
-	 public String getAllUser(Map<String, Object> modelMap){  
-	  modelMap.put("userList", users.findAllUsers());
-	  return "userlist";
-	 }
-	 
-	 
-	 @RequestMapping(value = "userEdit", method = RequestMethod.GET)
-	 public String editUser(Map<String, Object> modelMap){  
-		 
-	  modelMap.put("userList", users.findAllUsers());
-	  return "userEdit";
-	 }
-	 
+	@Autowired
+	private UserManager users;
+
+	@RequestMapping(value = "userlist", method = RequestMethod.GET)
+	public String getAllUser(Map<String, Object> modelMap) {
+		modelMap.put("userList", users.findAllUsers());
+		return "userlist";
+	}
+
+	@RequestMapping(value = "userEdit", method = RequestMethod.GET)
+	public String editUser(@RequestParam("usname1") String usname,
+			Map<String, Object> modelMap) {
+		modelMap.put("userList", users.findAllUsers());
+		return "userEdit";
 
 	}
+
+	@RequestMapping(value = "userEdit1", method = RequestMethod.GET)
+	public String editUser1(Map<String, Object> modelMap) {
+
+		modelMap.put("userList", users.findAllUsers());
+		return "userEdit";
+	}
+
+}
