029d93016cc16ededfe9dce9ad4b6fe0e953390e
Taras <martin15331@rambler.ru>
2014-02-01 20:08:33 +0200

    add delete button!!!

--- src/main/java/com/ita/edu/softserve/manager/impl/StationsManagerImpl.java
+++ src/main/java/com/ita/edu/softserve/manager/impl/StationsManagerImpl.java
@@ -3,12 +3,16 @@ package com.ita.edu.softserve.manager.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.NoResultException;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ita.edu.softserve.dao.AbstractDAO;
 import com.ita.edu.softserve.dao.StationsDAO;
+import com.ita.edu.softserve.entity.Lines;
 import com.ita.edu.softserve.entity.Stations;
 import com.ita.edu.softserve.manager.ManagerFactory;
 import com.ita.edu.softserve.manager.StationsManager;
@@ -16,17 +20,19 @@ import com.ita.edu.softserve.manager.StationsManager;
 /**
  * This is station service class.
  * 
- * @author Roman
+ * @author admin
  * 
  */
 @Service("stationsService")
 public class StationsManagerImpl implements StationsManager {
 
+	private static final Logger LOGGER = Logger.getLogger(Stations.class);
+
 	/**
 	 * Class to get access to DAO layer.
 	 */
 	@Autowired
-	private StationsDAO stationDao; 
+	private StationsDAO stationDao;
 
 	/**
 	 * Constructor without arguments.
@@ -46,56 +52,66 @@ public class StationsManagerImpl implements StationsManager {
 	/**
 	 * @return list of all stations.
 	 * 
-	 * @author Roman
 	 */
-	@SuppressWarnings("unchecked")
 	@Transactional
 	@Override
 	public List<Stations> findAllStations() {
-		return ((AbstractDAO<Stations>) stationDao).getAllEntities();
+		return stationDao.getAllEntities();
 	}
-	
+
 	/**
 	 * @return Station found by ID.
 	 */
+	@Transactional
 	@Override
 	public Stations findStationsById(int id) {
 		return stationDao.findById(id);
 	}
+
 	/**
-	 *  Save Stations in database.
+	 * Save Stations in database.
 	 */
+	@Transactional
 	@Override
 	public void saveStations(Stations... station) {
-		
-		for (Stations stop: station)
-		stationDao.save(stop);
+
+		for (Stations stations : station)
+			stationDao.save(stations);
 	}
-	
+
 	/**
-	 *  Remove Stations from database.
+	 * Remove Stations by Id from database.
 	 */
+	@Transactional
 	@Override
-	public void removeStations(Stations... station) {
-		
-		for (Stations stop: station)
-		stationDao.remove(stop);
+	public void removeStations(Integer stationId) {
+
+		Stations station = null;
+		try {
+			station = (Stations) stationDao.findById(stationId);
+			stationDao.remove(station);
+		} catch (NoResultException e) {
+			LOGGER.error("No such station!", e);
+		}
 	}
+
 	/**
-	 * Update database and get list of all stations.
+	 * Update Stations table in DB and get list of all stations.
 	 * 
 	 * @return list of all stations.
 	 */
+	@Transactional
 	@Override
 	public List<Stations> updateStations(Stations... station) {
+
 		List<Stations> stationUpdateResult = new ArrayList<Stations>();
-		for (Stations stop : station) {
-			stationUpdateResult.add((Stations) stationDao.update(stop));
+		for (Stations stations : station) {
+			stationUpdateResult.add((Stations) stationDao.update(stations));
 		}
 		return stationUpdateResult;
-	
 	}
+
 	public static StationsManager getInstance() {
-		return ManagerFactory.getManager(StationsManager.class); 
+		return ManagerFactory.getManager(StationsManager.class);
 	}
 }
--- src/main/webapp/jsp/stations.jsp
+++ src/main/webapp/jsp/stations.jsp
@@ -2,32 +2,35 @@
 	pageEncoding="UTF8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<!-- <html> -->
-<!-- <head> -->
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF8"> -->
-<!-- <title>Insert title here</title> -->
-<!-- </head> -->
-<!-- <body> -->
-<div id="context">
-	<%
-		int i = 0;
-	%>
-	<table border="1">
-		<tr>
-			<td>Number</td>
-			<td>Station Code</td>
-			<td>Station Name</td>
-
-		</tr>
-		<c:forEach var="station" items="${stationsList}">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
+<title>Insert title here</title>
+<link href="<c:url value="/resources/css/style.css" />" rel="stylesheet"
+	type="text/css" />
+</head>
+<body>
+	<div id="context">
+		<%
+			int i = 0;
+		%>
+		<table border="1">
 			<tr>
-				<td><%=++i%></td>
-				<td>${station.getStationCode()}</td>
-				<td>${station.getStationName()}</td>
+				<td align="center">Number</td>
+				<td align="center">Station Code</td>
+				<td align="center">Station Name</td>
 
 			</tr>
-		</c:forEach>
-	</table>
+			<c:forEach var="station" items="${stationsList}">
+				<tr>
+					<td align="center"><%=++i%></td>
+					<td align="center">${station.getStationCode()}</td>
+					<td align="center">${station.getStationName()}</td>
+					<!--	<td><a href="userEdit">Edit</a>  -->
+					<td><a href="delete/${station.getStationId()}">Delete</a></td>
+				</tr>
+			</c:forEach>
+		</table>
 	</div>
-<!-- </body> -->
-<!-- </html> -->
+</body>
+</html>
