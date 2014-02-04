3d81d91d9b086b55775711bf2664afdbf0c3dd74
tranzero <tranzero@192.168.0.5>
2014-02-04 14:15:37 +0200

    add  tryCatch block to PostForMainPageManager

--- src/main/java/com/ita/edu/softserve/manager/PostForMainPageManager.java
+++ src/main/java/com/ita/edu/softserve/manager/PostForMainPageManager.java
@@ -10,16 +10,16 @@ import com.ita.edu.softserve.exception.PostManagerExeption;
 @Service
 public interface PostForMainPageManager extends BaseManager {
 	
-	public List<Post> findPostList();
+	public List<Post> findPostList() throws PostManagerExeption;
 
-	public void createNews(String newsTitle, String newsDescription);
+	public void createNews(String newsTitle, String newsDescription) throws PostManagerExeption;
 
-	public void removeNews(Integer id);
+	public void removeNews(Integer id) throws PostManagerExeption;
 
-	public Post findNews(Integer postId);
+	public Post findNews(Integer postId) throws PostManagerExeption;
 
 	public void updateNews(Integer newsId, String newsTitle,
-			String newsDescription);
+			String newsDescription) throws PostManagerExeption;
 	public long getPostListCount() throws PostManagerExeption;
 
 }
