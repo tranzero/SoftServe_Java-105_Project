3d02ab13c43210e2516362780be804e488e138a5
tranzero <tranzero@192.168.0.5>
2014-02-04 15:29:01 +0200

    edit PaginationManagerImpl

--- src/main/java/com/ita/edu/softserve/manager/impl/PaginationManagerImpl.java
+++ src/main/java/com/ita/edu/softserve/manager/impl/PaginationManagerImpl.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Service;
 
 import com.ita.edu.softserve.manager.PaginationManager;
 
-
+ 
 
 @Service
 public class PaginationManagerImpl implements PaginationManager{
-	
+	private final int defaultPageCount = 1;
+	private final int defaultPageNumber = 1;
 	
 	@Override
 	public int getMaxPageCount(int resultsPerPage, long resultCount) {
 		if(resultsPerPage <= 0) {
-			resultsPerPage = 15;
+			resultsPerPage = this.getDefaultPageCount();
 		}
 		return (int) Math.ceil((double)resultCount / resultsPerPage);
 	}
@@ -23,10 +24,25 @@ public class PaginationManagerImpl implements PaginationManager{
 	@Override
 	public int getCurrentPagingPosition(int pageNumber, int resultsPerPage) {
 		if (pageNumber <= 0) {
-		    return 1;
+		    return this.getDefaultPageNumber();
 		}
 
 		return pageNumber * resultsPerPage;
+	}
+
+	
+	/**
+	 * @return the defaultPageCount
+	 */
+	public int getDefaultPageCount() {
+		return defaultPageCount;
+	}
+
+	/**
+	 * @return the defaultPageNumber
+	 */
+	public int getDefaultPageNumber() {
+		return defaultPageNumber;
 	}
 
 }
