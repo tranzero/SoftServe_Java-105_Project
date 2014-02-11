2feb7025e45f2c1efee90a7a8532fa362b5f9e7b
Anastasiia <Anastasiia@Anastasiia-PC>
2014-02-11 21:43:59 +0200

    fix bugs in LinesManager

--- src/main/java/com/ita/edu/softserve/manager/impl/LinesManagerImpl.java
+++ src/main/java/com/ita/edu/softserve/manager/impl/LinesManagerImpl.java
@@ -112,20 +112,13 @@ public class LinesManagerImpl implements LinesManager {
 	
 	@Override
 	public int getLinesByStationCount(String stationName){
-		Stations station = stationDao.findByStations(stationName).get(0);
-		return stlDao.getLinesByStationCount(station
-				.getStationId());
+	
+		return lineDao.getLinesByStationNameCount(stationName);
 	}
 
 	@Override
 	public List<Lines> getLinesByStationForPage(int from,int count, String stationName){
-		Stations station = stationDao.findByStations(stationName).get(0);
-		List<StationsOnLine> stlList = stlDao.getLinesByStationForOnePage(from, count,station
-				.getStationId());
-		List<Lines> linesList = new ArrayList<Lines>();
-		for (StationsOnLine stl : stlList) {
-			linesList.add(stl.getLineId());
-		}
+		List<Lines> linesList = lineDao.getLinesByStationForOnePage(from, count, stationName);
 		return linesList;
 	}
 	/**
