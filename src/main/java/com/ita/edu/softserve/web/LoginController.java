26f4bf02e8a548d5f21e4e4031f2f57b012c642e
Mik <Mik@pc>
2014-02-12 16:57:34 +0200

    Add changes to login process and security

--- src/main/java/com/ita/edu/softserve/web/LoginController.java
+++ src/main/java/com/ita/edu/softserve/web/LoginController.java
@@ -1,42 +1,32 @@
 package com.ita.edu.softserve.web;
 
 import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.ita.edu.softserve.entity.Users;
-import com.ita.edu.softserve.manager.UserManager;
-
 import org.springframework.security.core.userdetails.User;
 
 @Controller
-public class LoginController {
-	
-	@Autowired
-	private UserManager usersmanager;
+public class LoginController {	
 	
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String login(Map<String, Object> modelMap) {
 		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		String name = user.getUsername();
 		modelMap.put("username", name);		
-		System.out.println("SIGNED IN");
-		return "orders"; 
+		return "mainpage"; 
 	}
 	
 	@RequestMapping(value="/loginfailed")
 	public String loginerror(Map<String, Object> modelMap) {
 		modelMap.put("error", "true");		
-		return "orders";
- 	}
+		return "mainpage";
+	}
+	
 	
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
-	public String logout(Map<String, Object> modelMap) {
-		return "orders";
+	public String logout(Map<String, Object> modelMap) {		
+		return "mainpage";
  	}	
 }
