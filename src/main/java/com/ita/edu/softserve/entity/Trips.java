7135159b72965187534d9e172bd72e3a2dfc5b6d
dnycktc <denysnyckolskyyjob@gmail.com>
2014-02-13 00:59:47 +0200

    minor changes

--- src/main/java/com/ita/edu/softserve/entity/Trips.java
+++ src/main/java/com/ita/edu/softserve/entity/Trips.java
@@ -17,63 +17,124 @@ import javax.persistence.Table;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
+/**
+ * 
+ * @author dnycktc
+ * 
+ *         Entity class for trips
+ * 
+ */
 
 @Entity
 @Table(name = "trips")
 @NamedQueries({
-	@NamedQuery(name = Trips.TRIPS_FIND_ALL, query = Trips.TRIPS_FIND_ALL_QUERY),
-	@NamedQuery(name = Trips.TRIPS_FIND_COUNT, query = Trips.TRIPS_FIND_COUNT_QUERY),
-	@NamedQuery(name = Trips.FIND_BY_TRANSPORTID, query = Trips.FIND_BY_TRANSPORTID_QUERY)
-})
-public class Trips extends BaseEntity{
-	
+		@NamedQuery(name = Trips.TRIPS_FIND_ALL, query = Trips.TRIPS_FIND_ALL_QUERY),
+		@NamedQuery(name = Trips.TRIPS_FIND_COUNT, query = Trips.TRIPS_FIND_COUNT_QUERY),
+		@NamedQuery(name = Trips.FIND_BY_TRANSPORTID, query = Trips.FIND_BY_TRANSPORTID_QUERY) })
+public class Trips extends BaseEntity {
+
+	/**
+	 * Name of query which is used for selecting trips from DB. Compatible with
+	 * paging.
+	 */
 	public static final String TRIPS_FIND_ALL = "Trips.findAll";
+	/**
+	 * Query which is used for selecting trips from DB. Compatible with paging.
+	 */
 	public static final String TRIPS_FIND_ALL_QUERY = "SELECT tr FROM Trips tr";
 
+	/**
+	 * Name of query which is used for selecting count of trips from DB. Used in
+	 * paging.
+	 */
 	public static final String TRIPS_FIND_COUNT = "Trips.findCount";
+
+	/**
+	 * Query which is used for selecting count of trips from DB. Used in paging.
+	 */
 	public static final String TRIPS_FIND_COUNT_QUERY = "SELECT COUNT(tr.tripId) FROM Trips tr";
-	
+
+	/**
+	 * Name of query which is used for selecting trips relative to some
+	 * transport from DB.
+	 */
 	public static final String FIND_BY_TRANSPORTID = "Trips.findByTransportId";
+	/**
+	 * Query which is used for selecting trips relative to some transport from
+	 * DB.
+	 */
 	public static final String FIND_BY_TRANSPORTID_QUERY = "SELECT tr FROM Trips tr WHERE tr.transport.transportId = ?1";
 
-	
-	
+	/**
+	 * Identifier field
+	 */
+
 	@Id
 	@Column(name = "TRIPID")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer tripId;
-	
-	
+
+	/**
+	 * Transport, related to trip
+	 */
+
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "TRANSPORTID")
 	private Transports transport;
-	
-	
+
+	/**
+	 * Remaining quantity of seats (class 1)
+	 */
+
 	@Column(name = "REMSEATCLASS1", nullable = false)
 	private Integer remSeatClass1;
-	
-	
+
+	/**
+	 * Remaining quantity of seats (class 2)
+	 */
+
 	@Column(name = "REMSEATCLASS2", nullable = false)
 	private Integer remSeatClass2;
-	
-	
+
+	/**
+	 * Remaining quantity of seats (class 3)
+	 */
+
 	@Column(name = "REMSEATCLASS3", nullable = false)
 	private Integer remSeatClass3;
-	
-	
+
+	/**
+	 * Day when trip starts
+	 */
+
 	@Column(name = "STARTDATE", nullable = false)
 	private Date startDate;
-	
-	
-	
+
+	/**
+	 * Default constructor for trips
+	 */
+
 	public Trips() {
 		super();
 	}
 
-	
-	
-	public Trips(Transports transport, Integer remSeatClass1, Integer remSeatClass2,
-			Integer remSeatClass3, Date startDate) {
+	/**
+	 * Full arguments constructor for trips
+	 * 
+	 * @param transport
+	 *            Transport, related to trip
+	 * @param remSeatClass1
+	 *            Remaining quantity of seats (class 1)
+	 * @param remSeatClass2
+	 *            Remaining quantity of seats (class 2)
+	 * @param remSeatClass3
+	 *            Remaining quantity of seats (class 3)
+	 * @param startDate
+	 *            Day when trip starts
+	 */
+
+	public Trips(Transports transport, Integer remSeatClass1,
+			Integer remSeatClass2, Integer remSeatClass3, Date startDate) {
 		super();
 		this.transport = transport;
 		this.remSeatClass1 = remSeatClass1;
@@ -81,6 +142,15 @@ public class Trips extends BaseEntity{
 		this.remSeatClass3 = remSeatClass3;
 		this.startDate = startDate;
 	}
+
+	/**
+	 * Constructor with minimum arguments
+	 * 
+	 * @param transport
+	 *            Transport, related to trip
+	 * @param startDateDay
+	 *            when trip starts
+	 */
 
 	public Trips(Transports transport, Date startDate) {
 		super();
@@ -91,85 +161,102 @@ public class Trips extends BaseEntity{
 		this.startDate = startDate;
 	}
 
-
 	/**
 	 * @return the tripId
 	 */
 	public Integer getTripId() {
 		return tripId;
 	}
-//	/**
-//	 * @param tripId the tripId to set
-//	 */
-//	public void setTripId(Integer tripId) {
-//		this.tripId = tripId;
-//	}
+
 	/**
 	 * @return the transportId
 	 */
 	public Transports getTransport() {
 		return transport;
 	}
+
 	/**
-	 * @param transportId the transportId to set
+	 * @param transportId
+	 *            the transportId to set
 	 */
 	public void setTransport(Transports transport) {
 		this.transport = transport;
 	}
+
 	/**
 	 * @return the remSeatClass1
 	 */
 	public Integer getRemSeatClass1() {
 		return remSeatClass1;
 	}
+
 	/**
-	 * @param remSeatClass1 the remSeatClass1 to set
+	 * @param remSeatClass1
+	 *            the remSeatClass1 to set
 	 */
 	public void setRemSeatClass1(Integer remSeatClass1) {
 		this.remSeatClass1 = remSeatClass1;
 	}
+
 	/**
 	 * @return the remSeatClass2
 	 */
 	public Integer getRemSeatClass2() {
 		return remSeatClass2;
 	}
+
 	/**
-	 * @param remSeatClass2 the remSeatClass2 to set
+	 * @param remSeatClass2
+	 *            the remSeatClass2 to set
 	 */
 	public void setRemSeatClass2(Integer remSeatClass2) {
 		this.remSeatClass2 = remSeatClass2;
 	}
+
 	/**
 	 * @return the remSeatClass3
 	 */
 	public Integer getRemSeatClass3() {
 		return remSeatClass3;
 	}
+
 	/**
-	 * @param remSeatClass3 the remSeatClass3 to set
+	 * @param remSeatClass3
+	 *            the remSeatClass3 to set
 	 */
 	public void setRemSeatClass3(Integer remSeatClass3) {
 		this.remSeatClass3 = remSeatClass3;
 	}
+
 	/**
 	 * @return the startDate
 	 */
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	/**
-	 * @param startDate the startDate to set
+	 * @param startDate
+	 *            the startDate to set
 	 */
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+	
+	/**
+	 * Hash code function for trips
+	 */
 	
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder().append(transport).append(startDate)
 				.hashCode();
 	}
+
+	
+	/**
+	 * Equals function for trips
+	 */
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -181,11 +268,9 @@ public class Trips extends BaseEntity{
 			return false;
 		Trips other = (Trips) obj;
 
-		return new EqualsBuilder().append(transport, other.transport).append(startDate, other.startDate)
-				.isEquals();
+		return new EqualsBuilder().append(transport, other.transport)
+				.append(startDate, other.startDate).isEquals();
 	}
-
-
 
 	@Override
 	public String toString() {
--- src/main/java/com/ita/edu/softserve/manager/TripsManager.java
+++ src/main/java/com/ita/edu/softserve/manager/TripsManager.java
@@ -8,13 +8,44 @@ import com.ita.edu.softserve.entity.Trips;
 
 public interface TripsManager extends BaseManager {
 
+	/**
+	 * Returns complete list of trips
+	 * @return Complete list of trips
+	 */
 	List<Trips> getAllTrips();
+	/**
+	 * Returns list of trips using given limit 
+	 * @param firstElement Starting element number
+	 * @param count 
+	 * @return
+	 */
 	
 	List<Trips> getTripsForLimit(int firstElement, int count);
 	
+	/**
+	 * 
+	 * @param pageNumber
+	 * @param count
+	 * @return
+	 */
+	
 	public List<Trips> getTripsForPage(int pageNumber, int count);
 	
+	/**
+	 * 
+	 * @param locale
+	 * @param minDate
+	 * @param maxDate
+	 * @param transportId
+	 * @return
+	 */
+	
 	public boolean addTripsInInterval(Locale locale, String minDate, String maxDate, int transportId);
+	
+	/**
+	 * 
+	 * @return
+	 */
 	
 	long getTripsListCount();
 
