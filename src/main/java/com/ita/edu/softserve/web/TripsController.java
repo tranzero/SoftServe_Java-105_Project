7488e8f77bc600a8b672d5b418e327089408d5f4
dnycktc <denysnyckolskyyjob@gmail.com>
2014-02-12 10:19:52 +0200

    moved common parts of methods in trips controller into private method

--- src/main/java/com/ita/edu/softserve/web/TripsController.java
+++ src/main/java/com/ita/edu/softserve/web/TripsController.java
@@ -31,8 +31,6 @@ public class TripsController {
 	private static final String MANAGETRIPSPAGE_SPRING_NAME = "tripsmanagerpage";
 	private static final String TRIPSLIST_NAME = "tripsList";
 	private static final String DATEFORMAT_NAME = "dateFormat";
-	
-	
 
 	@Autowired
 	private TripsManager tripsManager;
@@ -40,10 +38,7 @@ public class TripsController {
 	private PaginationManager paginationManager = PaginationManager
 			.getInstance();
 
-	@RequestMapping(value = TRIPSPAGE_WEB_NAME, method = RequestMethod.GET)
-	public String printTripsPage(
-			@RequestParam(value = PaginationManager.PAGE_NUMBER_NAME, required = false) Integer pageNumber,
-			@RequestParam(value = PaginationManager.RESULTS_PER_PAGE_NAME, required = false) Integer resultsPerPage,
+	private void completeMapForTrips(Integer pageNumber, Integer resultsPerPage,
 			Map<String, Object> modelMap, Locale locale) {
 		long count = tripsManager.getTripsListCount();
 		PageInfoContainer container = new PageInfoContainer(pageNumber,
@@ -58,6 +53,14 @@ public class TripsController {
 				new SimpleDateFormat(
 						lang.equalsIgnoreCase(UKRAINIAN) ? UKRAINIAN_DATE_FORMAT
 								: DEFAULT_DATE_FORMAT));
+	}
+
+	@RequestMapping(value = TRIPSPAGE_WEB_NAME, method = RequestMethod.GET)
+	public String printTripsPage(
+			@RequestParam(value = PaginationManager.PAGE_NUMBER_NAME, required = false) Integer pageNumber,
+			@RequestParam(value = PaginationManager.RESULTS_PER_PAGE_NAME, required = false) Integer resultsPerPage,
+			Map<String, Object> modelMap, Locale locale) {
+		completeMapForTrips(pageNumber, resultsPerPage, modelMap, locale);
 		return TRIPSPAGE_SPRING_NAME;
 	}
 
@@ -66,61 +69,25 @@ public class TripsController {
 			@RequestParam(value = PaginationManager.PAGE_NUMBER_NAME, required = false) Integer pageNumber,
 			@RequestParam(value = PaginationManager.RESULTS_PER_PAGE_NAME, required = false) Integer resultsPerPage,
 			Map<String, Object> modelMap, Locale locale) {
-		long count = tripsManager.getTripsListCount();
-		PageInfoContainer container = new PageInfoContainer(pageNumber,
-				resultsPerPage, count);
-		paginationManager.validatePaging(container);
-		PagingController.deployPaging(modelMap, container, paginationManager);
-		String lang = locale.getLanguage();
-		modelMap.put(TRIPSLIST_NAME, tripsManager.getTripsForPage(
-				container.getPageNumber(), container.getResultsPerPage()));
-		modelMap.put(
-				DATEFORMAT_NAME,
-				new SimpleDateFormat(
-						lang.equalsIgnoreCase(UKRAINIAN) ? UKRAINIAN_DATE_FORMAT
-								: DEFAULT_DATE_FORMAT));
+		completeMapForTrips(pageNumber, resultsPerPage, modelMap, locale);
 		return TRIPS_SPRING_NAME;
 	}
-	
+
 	@RequestMapping(value = MANAGETRIPSPAGE_WEB_NAME, method = RequestMethod.GET)
 	public String printManageTripsPage(
 			@RequestParam(value = PaginationManager.PAGE_NUMBER_NAME, required = false) Integer pageNumber,
 			@RequestParam(value = PaginationManager.RESULTS_PER_PAGE_NAME, required = false) Integer resultsPerPage,
 			Map<String, Object> modelMap, Locale locale) {
-		long count = tripsManager.getTripsListCount();
-		PageInfoContainer container = new PageInfoContainer(pageNumber,
-				resultsPerPage, count);
-		paginationManager.validatePaging(container);
-		PagingController.deployPaging(modelMap, container, paginationManager);
-		String lang = locale.getLanguage();
-		modelMap.put(TRIPSLIST_NAME, tripsManager.getTripsForPage(
-				container.getPageNumber(), container.getResultsPerPage()));
-		modelMap.put(
-				DATEFORMAT_NAME,
-				new SimpleDateFormat(
-						lang.equalsIgnoreCase(UKRAINIAN) ? UKRAINIAN_DATE_FORMAT
-								: DEFAULT_DATE_FORMAT));
+		completeMapForTrips(pageNumber, resultsPerPage, modelMap, locale);
 		return MANAGETRIPSPAGE_SPRING_NAME;
 	}
-	
+
 	@RequestMapping(value = MANAGETRIPS_WEB_NAME, method = RequestMethod.GET)
 	public String printManageTrips(
 			@RequestParam(value = PaginationManager.PAGE_NUMBER_NAME, required = false) Integer pageNumber,
 			@RequestParam(value = PaginationManager.RESULTS_PER_PAGE_NAME, required = false) Integer resultsPerPage,
 			Map<String, Object> modelMap, Locale locale) {
-		long count = tripsManager.getTripsListCount();
-		PageInfoContainer container = new PageInfoContainer(pageNumber,
-				resultsPerPage, count);
-		paginationManager.validatePaging(container);
-		PagingController.deployPaging(modelMap, container, paginationManager);
-		String lang = locale.getLanguage();
-		modelMap.put(TRIPSLIST_NAME, tripsManager.getTripsForPage(
-				container.getPageNumber(), container.getResultsPerPage()));
-		modelMap.put(
-				DATEFORMAT_NAME,
-				new SimpleDateFormat(
-						lang.equalsIgnoreCase(UKRAINIAN) ? UKRAINIAN_DATE_FORMAT
-								: DEFAULT_DATE_FORMAT));
+		completeMapForTrips(pageNumber, resultsPerPage, modelMap, locale);
 		return MANAGETRIPS_SPRING_NAME;
 	}
 }
---
+++ src/main/webapp/jsp/addTripPage.jsp
@@ -0,0 +1,89 @@
+<%@ page language="java" contentType="text/html; charset=UTF8"
+	pageEncoding="UTF8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<table style="align: center">
+	<thead>
+		<tr>
+			<th><spring:message code="label.transport.transportcode" /></th>
+			<th><spring:message code="label.transport.starttime" /></th>
+			<th><spring:message code="label.routes.routecode" /></th>
+			<th><spring:message code="label.lines.linename" /></th>
+			<th><spring:message code="label.transport.seatclass1" /></th>
+			<th><spring:message code="label.transport.seatclass2" /></th>
+			<th><spring:message code="label.transport.seatclass3" /></th>
+			<th><spring:message code="label.transport.genprice" /></th>
+		</tr>
+	</thead>
+	<tbody>
+		<c:forEach items="${transportsList}" var="transport">
+			<tr>
+				<td align="center">${transport.getTransportCode()}</td>
+				<td align="center">${transport.getStartTime()}</td>
+				<td align="center">${transport.getRoutes().getRouteCode()}</td>
+				<td align="center"><a
+					href="getsLineId/${transport.getRoutes().getLineId().getLineId()}">${transport.getRoutes().getLineId().getLineName()}</a></td>
+				<td align="center">${transport.getSeatclass1()}</td>
+				<td align="center">${transport.getSeatclass2()}</td>
+				<td align="center">${transport.getSeatclass3()}</td>
+				<td align="center">${transport.getGenPrice()}</td>
+				<td align="center"><a
+					href="editTransport/${transport.getTransportId()}"> <input
+						id="edit" type="button" name="edit"
+						value="<spring:message code="label.edit"/>">
+				</a></td>
+
+				<td align="center"><a
+					href="removeTransport/${transport.getTransportId()}"> <input
+						id="delete" type="button" name="delete"
+						onclick="return confirm_delete()"
+						value="<spring:message code="label.delete"/>">
+				</a></td>
+		</c:forEach>
+	</tbody>
+</table>
+<div class="pagination">
+	<ul class="bootpag">
+		<c:if test="${pageNumber>1}">
+			<li class="prev"><a href="javascript:void(0);"
+				onclick="showTransportPage(1, ${resultsPerPage})"> « </a></li>
+			<li class="prev"><a href="javascript:void(0);"
+				onclick="showTransportPage(${pageNumber-1},${resultsPerPage})"> <spring:message
+						code="label.prev" />
+			</a></li>
+		</c:if>
+		<c:if test="${pageNumber==1}">
+			<li class="prev disabled"><a href="javascript:void(0);"> « </a></li>
+			<li class="prev disabled"><a href="javascript:void(0);"> <spring:message
+						code="label.prev" />
+			</a></li>
+		</c:if>
+		<c:forEach var="i" begin="${firstPage}" end="${lastPage}" step="1"
+			varStatus="status">
+			<c:if test="${pageNumber!=i}">
+				<li><a href="javascript:void(0);"
+					onclick="showTransportPage(${i},${resultsPerPage})"> ${i} </a></li>
+			</c:if>
+			<c:if test="${pageNumber==i}">
+				<li class="disabled"><a href="javascript:void(0);"> ${i} </a></li>
+			</c:if>
+		</c:forEach>
+
+		<c:if test="${pageNumber<maxPages}">
+			<li class="next"><a href="javascript:void(0);"
+				onclick="showTransportPage(${pageNumber+1},${resultsPerPage})"> <spring:message
+						code="label.next" />
+			</a></li>
+			<li class="next"><a href="javascript:void(0);"
+				onclick="showTransportPage(${maxPages},${resultsPerPage})"> » </a></li>
+		</c:if>
+		<c:if test="${pageNumber==maxPages}">
+			<li class="next disabled"><a href="javascript:void(0);"> <spring:message
+						code="label.next" />
+			</a></li>
+			<li class="next disabled"><a href="javascript:void(0);"> » </a></li>
+		</c:if>
+	</ul>
+</div>
+
