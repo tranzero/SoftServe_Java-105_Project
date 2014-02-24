31a626d138b0ff7842f3ebecf44551130842e055
rparatc <RomeXak@gmail.com>
2014-02-24 23:28:08 +0200

    Fixed validation.

--- src/main/java/com/ita/edu/softserve/web/TransportsValidator.java
+++ src/main/java/com/ita/edu/softserve/web/TransportsValidator.java
@@ -1,8 +1,11 @@
 package com.ita.edu.softserve.web;
 
+import com.ita.edu.softserve.utils.ParseUtil;
+
+import java.sql.Time;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.ita.edu.softserve.entity.Transports;
@@ -10,31 +13,22 @@ import com.ita.edu.softserve.entity.Transports;
 @Component("transportsValidator")
 public class TransportsValidator implements Validator {
 
+	private static final String TRANSPORT_CODE_PATERN = "^[a-zA-Z0-9]{5,15}$";
+	private static final String START_TIME_PATERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+
 	private static final String TRANSPORT_CODE = "transportCode";
 	private static final String START_TIME = "startTime";
 	private static final String SEATCLASS1 = "seatclass1";
 	private static final String SEATCLASS2 = "seatclass2";
 	private static final String SEATCLASS3 = "seatclass3";
 	private static final String GENERAL_PRICE = "genPrice";
-	
-	private static final String TRANSPORT_CODE_EMPTY = "transportCode.empty";
-	private static final String START_TIME_EMPTY = "startTime.empty";
-	private static final String SEATCLASS1_EMPTY = "seatclass1.empty";
-	private static final String SEATCLASS2_EMPTY = "seatclass2.empty";
-	private static final String SEATCLASS3_EMPTY = "seatclass3.empty";
-	private static final String GEN_PRICE_EMPTY = "genPrice.empty";
-	private static final String SEATCLASS2_REQUIRED = "seatclass2.required";
-	private static final String SEATCLASS3_REQUIRED = "seatclass3.required";
-	private static final String GEN_PRICE_REQUIRED = "genPrice.required";
-	private static final String SEATCLASS1_REQUIRED = "seatclass1.required";
 
-	private static final String EMPTY_TRANSPORT_CODE_MESSAGE = "Empty transport Code";
-	private static final String EMPTY_START_TIME_MESSAGE = "Empty start Time";
-	private static final String EMPTY_SEAT_CLASS_1_MESSAGE = "Empty seat class 1";
-	private static final String EMPTY_SEAT_CLASS_2_MESSAGE = "Empty seat class 2";
-	private static final String EMPTY_SEAT_CLASS_3_MESSAGE = "Empty seat class 3";
-	private static final String EMPTY_GENERAL_PRICE_MESSAGE = "Empty general Price";
-	
+	private static final String SEATCLASS_REQUIRED = "seatclass.required";
+	private static final String GEN_PRICE_REQUIRED = "genPrice.required";
+	private static final String TRANSPORT_CODE_MATCHER = "transportCode.matcher";
+	private static final String START_TIME_MATCHER = "startTime.matcher";
+	private static final String START_TIME_NULL = "startTime.null";
+
 	/**
 	 * This Validator validates *just* Transports instance
 	 */
@@ -51,40 +45,95 @@ public class TransportsValidator implements Validator {
 	public void validate(Object obj, Errors error) {
 		Transports transport = (Transports) obj;
 
-		ValidationUtils.rejectIfEmpty(error, TRANSPORT_CODE,
-				TRANSPORT_CODE_EMPTY, EMPTY_TRANSPORT_CODE_MESSAGE);
-		ValidationUtils.rejectIfEmpty(error, START_TIME, START_TIME_EMPTY,
-				EMPTY_START_TIME_MESSAGE);
+		validateTransportCode(transport.getTransportCode(), error);
 
-		ValidationUtils.rejectIfEmpty(error, SEATCLASS1, SEATCLASS1_EMPTY,
-				EMPTY_SEAT_CLASS_1_MESSAGE);
-		ValidationUtils.rejectIfEmpty(error, SEATCLASS2, SEATCLASS2_EMPTY,
-				EMPTY_SEAT_CLASS_2_MESSAGE);
-		ValidationUtils.rejectIfEmpty(error, SEATCLASS3, SEATCLASS3_EMPTY,
-				EMPTY_SEAT_CLASS_3_MESSAGE);
-		ValidationUtils.rejectIfEmpty(error, GENERAL_PRICE, GEN_PRICE_EMPTY,
-				EMPTY_GENERAL_PRICE_MESSAGE);
+		validateStartTime(transport.getStartTime(), error);
 
-		if (transport.getSeatclass1() == 0) {
-			error.rejectValue(SEATCLASS1, SEATCLASS1_REQUIRED);
+		validateSeatClasses(transport.getSeatclass1(),
+				transport.getSeatclass2(), transport.getSeatclass3(), error);
+
+		validateGeneralPrice(transport.getGenPrice(), error);
+
+	}
+
+	/**
+	 * String must contain only letter and digits more then 5 and less then 15.
+	 * 
+	 * @param transportCode
+	 *            The transport code to match.
+	 * @param error
+	 *            the Errors object that stores and exposes information about
+	 *            data-binding and validation errors for a specific object.
+	 */
+	private void validateTransportCode(String transportCode, Errors error) {
+		if (!transportCode.matches(TRANSPORT_CODE_PATERN)) {
+			error.rejectValue(TRANSPORT_CODE, TRANSPORT_CODE_MATCHER);
+		}
+	}
+
+	/**
+	 * @param startTime
+	 *            The start time to match.
+	 * @param error
+	 *            the Errors object that stores and exposes information about
+	 *            data-binding and validation errors for a specific object.
+	 */
+	private void validateStartTime(Time startTime, Errors error) {
+		if (startTime == null) {
+			error.rejectValue(START_TIME, START_TIME_NULL);
+		} else {
+			String time = ParseUtil.parseTimeToString(startTime);
+			if (!time.matches(START_TIME_PATERN)) {
+				error.rejectValue(START_TIME, START_TIME_MATCHER);
+			}
+		}
+	}
+
+	/**
+	 * Verifies if seat classes are not less then 0 or equals to 0. Must be
+	 * entered number of seats at least one class.
+	 * 
+	 * @param seatClass1
+	 *            the seat class 1 to check.
+	 * @param seatClass2
+	 *            the seat class 2 to check.
+	 * @param seatClass3
+	 *            the seat class 3 to check.
+	 * @param error
+	 *            the Errors object that stores and exposes information about
+	 *            data-binding and validation errors for a specific object.
+	 */
+	private void validateSeatClasses(int seatClass1, int seatClass2,
+			int seatClass3, Errors error) {
+
+		boolean bool = ((seatClass1 > 0) || (seatClass2 > 0) || (seatClass3 > 0)) ? false
+				: true;
+
+		if ((seatClass1 <= 0) && bool) {
+			error.rejectValue(SEATCLASS1, SEATCLASS_REQUIRED);
 		}
 
-		if (transport.getSeatclass2() == 0) {
-			error.rejectValue(SEATCLASS2, SEATCLASS2_REQUIRED);
+		if ((seatClass2 <= 0) && bool) {
+			error.rejectValue(SEATCLASS2, SEATCLASS_REQUIRED);
 		}
 
-		if (transport.getSeatclass3() == 0) {
-			error.rejectValue(SEATCLASS3, SEATCLASS3_REQUIRED);
+		if ((seatClass3 <= 0) && bool) {
+			error.rejectValue(SEATCLASS3, SEATCLASS_REQUIRED);
 		}
+	}
 
-		if (transport.getGenPrice() == 0.0) {
+	/**
+	 * Verifies if price is not less then 0 or equals to 0.
+	 * @param genPrice
+	 *            price to check.
+	 * @param error
+	 *            the Errors object that stores and exposes information about
+	 *            data-binding and validation errors for a specific object.
+	 */
+	private void validateGeneralPrice(double genPrice, Errors error) {
+
+		if (genPrice <= 0.0) {
 			error.rejectValue(GENERAL_PRICE, GEN_PRICE_REQUIRED);
-		}
-
-		try {
-			transport.getSeatclass1();
-		} catch (NumberFormatException e) {
-			error.rejectValue(SEATCLASS1, SEATCLASS1_REQUIRED);
 		}
 	}
 }
