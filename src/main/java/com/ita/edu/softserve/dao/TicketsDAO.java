391fe2ddd1b65b2b562f893fdf256679c2fc0d8e
Anastasiia <Anastasiia@Anastasiia-PC>
2014-03-06 01:22:51 +0200

    Add comments

--- src/main/java/com/ita/edu/softserve/dao/TicketsDAO.java
+++ src/main/java/com/ita/edu/softserve/dao/TicketsDAO.java
@@ -6,5 +6,19 @@ import java.util.List;
 import com.ita.edu.softserve.entity.Tickets;
 
 public interface TicketsDAO extends AbstractDAOIface<Tickets>{
+	
+	/**
+	 * 
+	 * @param id of order
+	 * @return  list of tickets with certain order id
+	 */
 	List<Tickets> findTicketsByOrderId(Integer id);
+	
+	/**
+	 * 
+	 * @param id of trip
+	 * @return  list of tickets with certain trip id
+	 */
+	List<Tickets> findTicketsByTripId(Integer id);
+	
 }
