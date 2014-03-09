61209a63b1ea7c699b3ffd01f7e0dacfcb53ca62
rparatc <RomeXak@gmail.com>
2014-03-09 12:13:39 +0200

    Added Transport Criteria Container class to get information about criteria on page.

---
+++ src/main/java/com/ita/edu/softserve/validationcontainers/TransportCriteriaContainer.java
@@ -0,0 +1,50 @@
+package com.ita.edu.softserve.validationcontainers;
+
+public interface TransportCriteriaContainer {
+
+	void setValuableParams(String transportCode, String routeName,
+			String routesCode, Integer seatClass1, Integer seatClass2,
+			Integer seatClass3, String priceName, String orderByCriteria,
+			String orderByDirection);
+
+	String getTransportCode();
+
+	void setTransportCode(String transportCode);
+
+	String getRouteName();
+
+	void setRouteName(String routeName);
+
+	String getRoutesCode();
+
+	void setRoutesCode(String routesCode);
+
+	Integer getSeatClass1();
+
+	void setSeatClass1(Integer seatClass1);
+
+	Integer getSeatClass2();
+
+	void setSeatClass2(Integer seatClass2);
+
+	Integer getSeatClass3();
+
+	void setSeatClass3(Integer seatClass3);
+
+	String getPriceName();
+
+	void setPriceName(String priceName);
+
+	Double getPrice();
+
+	void setPrice(Double price);
+
+	String getOrderByCriteria();
+
+	void setOrderByCriteria(String orderByCriteria);
+
+	String getOrderByDirection();
+
+	void setOrderByDirection(String orderByDirection);
+
+}
