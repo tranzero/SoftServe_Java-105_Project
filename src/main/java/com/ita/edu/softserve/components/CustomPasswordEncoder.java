b588755c2b27dc554131906a068c816be8899f59
Mik <Mik@pc>
2014-03-27 08:22:02 +0200

    password encoder once more

--- src/main/java/com/ita/edu/softserve/components/CustomPasswordEncoder.java
+++ src/main/java/com/ita/edu/softserve/components/CustomPasswordEncoder.java
@@ -1,5 +1,12 @@
 package com.ita.edu.softserve.components;
 
-public interface CustomPasswordEncoder {
+import com.ita.edu.softserve.entity.Users;
 
+public interface CustomPasswordEncoder {
+	
+	/**
+	 * Method for password encoding by using BCryptPasswordEncoder
+	 */
+	public void encodePassword(Users user);
+	
 }
