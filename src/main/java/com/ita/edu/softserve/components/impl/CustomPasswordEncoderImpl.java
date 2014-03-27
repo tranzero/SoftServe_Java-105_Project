fc45c6bfebaceb322f50075d72c6e8fbf0a24234
Mik <Mik@pc>
2014-03-27 13:18:49 +0200

    condition

--- src/main/java/com/ita/edu/softserve/components/impl/CustomPasswordEncoderImpl.java
+++ src/main/java/com/ita/edu/softserve/components/impl/CustomPasswordEncoderImpl.java
@@ -24,8 +24,24 @@ public class CustomPasswordEncoderImpl implements CustomPasswordEncoder{
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String usrPassword = user.getPassword();
 		String hashedPassword = passwordEncoder.encode(usrPassword);
-		if (!user.getPassword().equals(usersmanage.findUser(user.getUserId()).getPassword())){
+		if (isSamePassword(user)){
 			user.setPassword(hashedPassword);
 		}
+	}
+	
+	/**
+	 * Condition to investigate if entered user password matches password in DB
+	 * 
+	 * @param user
+	 */	
+	private boolean isSamePassword(Users user) {
+		try {
+			if(!user.getPassword().equals(usersmanage.findUser(user.getUserId()).getPassword())){
+				return true;
+			}			
+		} catch (RuntimeException e) {
+			return true;
+		}
+		return false;
 	}
 }
