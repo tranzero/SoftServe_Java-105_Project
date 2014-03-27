d258633b1e3f24320861e9a6cd712290840ecc2d
Mik <Mik@pc>
2014-03-27 12:57:08 +0200

    fixed rehasing

--- src/main/java/com/ita/edu/softserve/components/impl/CustomPasswordEncoderImpl.java
+++ src/main/java/com/ita/edu/softserve/components/impl/CustomPasswordEncoderImpl.java
@@ -1,15 +1,20 @@
 package com.ita.edu.softserve.components.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.ita.edu.softserve.components.CustomPasswordEncoder;
 import com.ita.edu.softserve.entity.Users;
+import com.ita.edu.softserve.manager.UserManager;
 
 @Component
 public class CustomPasswordEncoderImpl implements CustomPasswordEncoder{
 	
+	@Autowired
+	private UserManager usersmanage;
+
 	/**
 	 * Method for password encoding by using BCryptPasswordEncoder
 	 * 
@@ -18,7 +23,9 @@ public class CustomPasswordEncoderImpl implements CustomPasswordEncoder{
 	public void encodePassword(Users user) {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String usrPassword = user.getPassword();
-		String hashedPassword = passwordEncoder.encode(usrPassword);		
-		user.setPassword(hashedPassword);			
+		String hashedPassword = passwordEncoder.encode(usrPassword);
+		if (!user.getPassword().equals(usersmanage.findUser(user.getUserId()).getPassword())){
+			user.setPassword(hashedPassword);
+		}
 	}
 }
